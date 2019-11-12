68eee1b1c571220417c3c9a43c73a0952c535889
Leonel Bongiovanni <labongiovanni@live.com.ar>
2019-11-12 12:01:08 -0300

    Agregada configuracion para enviar Correos
    Tanto de Verificacion de correo como de cambio de contraseña

--- app/User.php
+++ app/User.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
 {
     use Notifiable;
 
